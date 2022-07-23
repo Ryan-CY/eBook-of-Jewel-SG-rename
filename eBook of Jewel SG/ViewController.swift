bde2d4e640ff65ece5c8abc3ee4db4434ffb68a7
Ryan CY <phillin820109@msn.com>
2022-07-23 12:16:03 +0800

    Initial Commit

---
+++ eBook of Jewel SG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eBook of Jewel SG
+//
+//  Created by Ryan Lin on 2022/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
